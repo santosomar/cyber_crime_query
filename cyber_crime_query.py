6e41b0e731fbcd70e37a78a3253cad10238129c7
Omar Santos <santosomar@gmail.com>
2018-05-08 18:37:46 -0400

    updating script

---
+++ cyber_crime_query.py
@@ -0,0 +1,10 @@
+#!/bin/python
+
+from cybercrimetracker.cybercrimeTrackerAPI import cybercrimeTrackerAPI
+import sys
+import json
+
+query = ''
+if len(sys.argv) > 1:
+   query = sys.argv[1]
+print(json.dumps(cybercrimeTrackerAPI().search(query), indent=2))
